92a2782370ce417009b7cf4588b0e43240b4f80f
Karl Berger <hekaber.sw@gmail.com>
2022-01-10 18:37:23 +0100

    [TASK] Apply changes on menu and teaser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServicesComponent } from './components/services/services.component';
 import { TeaserComponent } from './components/teaser/teaser.component';
-import { TeamComponent } from './components/team/team.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { MapComponent } from './components/map/map.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -17,10 +15,8 @@ import { FooterComponent } from './components/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    CarouselComponent,
     ServicesComponent,
     TeaserComponent,
-    TeamComponent,
     ContactComponent,
     MapComponent,
     FooterComponent
